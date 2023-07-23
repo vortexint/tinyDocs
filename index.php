224b23e2c336d819caea9f2181021490ec2c6d50
Igor Alexey <igor@vortex-dev.com>
2023-07-23 17:39:51 -0300

    layout progress

--- index.php
+++ index.php
@@ -21,21 +21,21 @@ $additionalBody = '';
 
 // load the selected modules' *.php files
 foreach ($modules as $module) {
-  // include the .php file in modules/$module/
-  include_once 'modules/'. $module. '/main.php';
+    // include the .php file in modules/$module/
+    include_once 'modules/' . $module . '/main.php';
 }
 
-function parse_md_page() {
-  if (isset($_GET['page'])) {
-    $pageValue = $_GET['page'];
-    if ($pageValue == "index") {
+function create_page_contents()
+{
+    if (isset($_GET['page'])) {
+        $pageValue = $_GET['page'];
+        if ($pageValue == "index") {
 
+        }
+    } else {
+        // No 'page' parameter found in the URL redirect to ?page=Main
+        header("Location:?page=index");
     }
-  }
-  else {
-    // No 'page' parameter found in the URL redirect to ?page=Main
-    header("Location:?page=index");
-  }
 }
 ?>
 
@@ -43,26 +43,34 @@ function parse_md_page() {
 <html>
 
 <head>
-  <title>
-    <?php echo $title; ?>
-  </title>
-  <link rel="icon" type="image/x-icon" href="<?php echo $favicon; ?>">
-  <link rel="stylesheet" href="styles/main.css">
-  <link rel="stylesheet" href="styles/themes/<?php echo $defaultTheme; ?>">
-  <meta name="keywords" content="<?php echo $description; ?>">
-  <?php
+    <title>
+        <?php echo $title; ?>
+    </title>
+    <link rel="icon" type="image/x-icon" href="<?php echo $favicon; ?>">
+    <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="styles/themes/<?php echo $defaultTheme; ?>">
+    <meta name="keywords" content="<?php echo $description; ?>">
+    <?php
     echo $additionalHead;
-  ?>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
-  <div class="content">
-    <?php
-      parse_md_page();
-      echo $additionalBody;
-    ?>
-    <p>Test</p>
-  </div>
+    <!-- Left column -->
+    <div><p>Test</p></div>
+    <!-- Middle column -->
+    <div>
+        <div class="content">
+        <p>Test</p>
+            <?php
+            create_page_contents();
+            echo $additionalBody;
+            ?>
+        </div>
+    </div>
+    <!-- Right column -->
+    <div><p>Test</p></div>
 </body>
+
 </html>
--- modules/highlight/main.php
+++ modules/highlight/main.php
@@ -1,7 +1,7 @@
 <?php
 $hljstheme = $ini['hljstheme'];
 // add hljs script and stylesheet to $additionalHead
-$additionalHead = $additionalHead . '<link rel="stylesheet" href="modules/highlight/styles' . $hljstheme . '">
+$additionalHead = $additionalHead . '<link rel="stylesheet" href="modules/highlight/styles/' . $hljstheme . '">
 <script src="modules/highlight/highlight.min.js"></script>
 '
 ?>
